5ba45f90e995cfb2efadc2370ae26185a4089296
aditi.shevale999@gmail.com <aditi.shevale999@gmail.com>
2018-02-27 12:11:09 +0530

    Modified

--- app/src/main/java/com/example/aditi/helloapp/MainActivity.java
+++ app/src/main/java/com/example/aditi/helloapp/MainActivity.java
@@ -4,23 +4,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button mButton;
+    Button mButton1 , mButton2;
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mButton = (Button) findViewById(R.id.button);
-        mButton.setOnClickListener(new View.OnClickListener() {
+        mTextView=(TextView) findViewById(R.id.textView);
+        mButton1 = (Button) findViewById(R.id.button);
+        mButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Button Pressed", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Button1", Toast.LENGTH_SHORT).show();
 
             }
         });
 
+
+    }
+    public  void  doSomething(View v){
+        Toast.makeText(this, "Button2", Toast.LENGTH_SHORT).show();
     }
 }
