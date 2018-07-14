3366c651d0098bc54a92a8df4a02ba09e1bce61a
aditishevale <aditi.shevale999@gmail.com>
2018-07-14 12:12:24 +0530

    First

--- app/src/main/java/com/example/aditi/helloapp/MainActivity.java
+++ app/src/main/java/com/example/aditi/helloapp/MainActivity.java
@@ -13,7 +13,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         mTextView=(TextView) findViewById(R.id.textView);
         mButton1 = (Button) findViewById(R.id.button);
